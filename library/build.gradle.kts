c75d55768171a1ce0032f28e6519fe36a572bc66
akhiltmohan <akhil.tmohan@airindia.com>
2024-07-25 12:45:37 +0530

    Init commit

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -40,7 +40,7 @@ publishing {
             from(components.findByName("release"))
             groupId = "com.atm.library"
             artifactId = "androidlibrary"
-            version = "1.0.4"
+            version = "1.0.5"
             afterEvaluate {
                 components.names.forEach {
                     println("Available component: $it")
